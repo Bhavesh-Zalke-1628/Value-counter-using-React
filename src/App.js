b2a4915fe383f6ec1aedad6aa69afa68aabdd0d2
Bhavesh Zalke <zalkebhavesh@gmail.com>
2023-09-19 09:16:56 +0530

    1 .5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import Basic from "./Basic";
-import Footer from "./Footer";
-import Header from "./Header";
+import { useState } from "react"
+import './App.css'
 function App() {
+  let [counter,setCounter] = useState(0)
+  // let counter = 11
+  const addValue =  () => {
+    if(counter < 20){
+      counter = counter+1
+      setCounter(counter)
+    }
+    else{
+      alert("Limit Reached")
+    }
+  } 
+  const removeValue =  () => {
+    if(counter>0){
+      counter = counter-1
+      setCounter(counter)
+    }
+    else{
+      alert("Do Not Descrese")
+    }
+  }   
   return (
     <>
-      <Header/>
-      <Basic/>
-      <Footer/>
+      <h1>Value Counter</h1>
+      <h2>Counter Value : {counter} </h2>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br/>
+      <button onClick={removeValue}>Remove Value {counter} </button>
     </>
   );
 }
